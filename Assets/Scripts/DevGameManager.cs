d769390f353d1c1a18fe78819611b61dbe6402dc
sthairno <45668272+sthairno@users.noreply.github.com>
2022-01-14 19:38:27 +0900

    シーン管理、マッチング機能を実装

--- Assets/Scripts/DevGameManager.cs
+++ Assets/Scripts/DevGameManager.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,35 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.VisualScripting;
 
-public class DevGameManager : MonoBehaviour
+public enum GamePhase
 {
-    private void Awake()
+    None,
+    MatchMaking,
+    EditScripts,
+    Battle,
+    GameOver
+}
+
+public enum Team : int
+{
+    Unassigned = 0,
+    Observer = 1,
+    TeamA = 2,
+    TeamB = 3,
+    TeamC = 4,
+    NPC = 5
+}
+
+public class DevGameManager : MonoBehaviourPunCallbacks
+{
+    public GamePhase CurrentPhase { get; private set; } = GamePhase.None;
+
+    private Dictionary<int, Team> teamAssignDic = new();
+
+    [SerializeField]
+    private GameObject matchMakingUI;
+
+    void Awake()
     {
         if (PhotonNetwork.IsMasterClient)
         {
@@ -20,6 +47,83 @@ public class DevGameManager : MonoBehaviour
                     Quaternion.identity);
             }
         }
+        StartMatchMakingPhase();
+    }
+
+    void Start()
+    {
+        AssignTeam(PhotonNetwork.LocalPlayer, Team.Observer);
+    }
+
+    void Update()
+    {
+        matchMakingUI.SetActive(CurrentPhase == GamePhase.MatchMaking);
+    }
+
+    public Team GetAssignedTeam(Player player)
+    {
+        return teamAssignDic.GetValueOrDefault(player.ActorNumber, Team.Unassigned);
+    }
+
+    public IEnumerable<(Player, Team)> GetAllAssignedTeam()
+    {
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            yield return (player, GetAssignedTeam(player));
+        }
+    }
+
+    public void AssignTeam(Player player, Team team)
+    {
+        if (CurrentPhase == GamePhase.MatchMaking)
+        {
+            photonView.RPC(nameof(AssignTeamImpl), RpcTarget.All, player.ActorNumber, (int)team);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            foreach (var (playerId, team) in teamAssignDic)
+            {
+                photonView.RPC(nameof(AssignTeamImpl), newPlayer, playerId, (int)team);
+            }
+        }
+    }
+
+    [PunRPC]
+    private void AssignTeamImpl(int playerId, int teamId)
+    {
+        teamAssignDic[playerId] = (Team)teamId;
+    }
+
+    private void StartMatchMakingPhase()
+    {
+        CurrentPhase = GamePhase.MatchMaking;
+    }
+
+    [PunRPC]
+    private void StartEditScriptsPhase()
+    {
+        CurrentPhase = GamePhase.EditScripts;
+    }
+
+    [PunRPC]
+    private void StartBattlePhase()
+    {
+        CurrentPhase = GamePhase.Battle;
+        CreateOwnPlayer();
+    }
+
+    [PunRPC]
+    private void GameOver()
+    {
+        CurrentPhase = GamePhase.GameOver;
+    }
+
+    private void CreateOwnPlayer()
+    {
         var player = PhotonNetwork.Instantiate(
             "DevPlayer",
             new(0, 0.5f, 0),
@@ -28,15 +132,4 @@ public class DevGameManager : MonoBehaviour
         script.enabled = true;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
