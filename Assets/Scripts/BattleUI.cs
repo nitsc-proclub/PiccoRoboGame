b73c8097fefbbda6e93fd2d2e33a56a39399b4fc
sthairno <45668272+sthairno@users.noreply.github.com>
2022-01-24 14:47:42 +0900

    ミニマップ、HPバーの実装

--- Assets/Scripts/BattleUI.cs
+++ Assets/Scripts/BattleUI.cs
@@ -1,12 +1,45 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.Presets;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class BattleUI : MonoBehaviour
 {
+    public bool MiniMapVisible
+    {
+        get => miniMap.activeInHierarchy;
+        set => miniMap.SetActive(value);
+    }
+
+    public Character LeftBaseCharacter = null;
+
+    public Character RightBaseCharacter;
+
+    [SerializeField]
+    GameObject miniMap;
+
+    [SerializeField]
+    GameObject leftTeamArea;
+
+    [SerializeField]
+    Text leftTeamText;
+
+    [SerializeField]
+    Image leftTeamHealthBar;
+
+    [SerializeField]
+    GameObject rightTeamArea;
+
+    [SerializeField]
+    Text rightTeamText;
+
+    [SerializeField]
+    Image rightTeamHealthBar;
+
     [SerializeField]
     Text timerText;
 
@@ -15,8 +48,6 @@ public class BattleUI : MonoBehaviour
 
     [SerializeField]
     LogText logTextPrefab;
-
-    int nextPushLogTime = 0;
 
     public void PushLog(string text)
     {
@@ -43,18 +74,39 @@ public class BattleUI : MonoBehaviour
             timerText.text = $"{min:0}:{sec:00}";
         }
 
-
-        if (millisec >= nextPushLogTime)
+        // HealthBar
         {
-            PushLog(
-                string.Concat(
-                    Enumerable.Repeat(
-                        "Hoge",
-                        Random.Range(1, 10)
-                    )
-                )
-            );
-            nextPushLogTime = millisec + Random.Range(400, 2000);
+            bool left = LeftBaseCharacter is not null;
+            leftTeamArea.SetActive(left);
+            if (left)
+            {
+                leftTeamText.text = LeftBaseCharacter.Display switch
+                {
+                    DisplayKind.None => "",
+                    DisplayKind.OwnerName => string.IsNullOrEmpty(PhotonNetwork.LocalPlayer.NickName)
+                        ? PhotonNetwork.LocalPlayer.UserId
+                        : PhotonNetwork.LocalPlayer.NickName,
+                    DisplayKind.Custom => LeftBaseCharacter.DisplayText,
+                    _ => ""
+                };
+                leftTeamHealthBar.fillAmount = Mathf.Clamp01((float)LeftBaseCharacter.HP / LeftBaseCharacter.MaxHP);
+            }
+
+            bool right = RightBaseCharacter is not null;
+            rightTeamArea.SetActive(right);
+            if (right)
+            {
+                rightTeamText.text = RightBaseCharacter.Display switch
+                {
+                    DisplayKind.None => "",
+                    DisplayKind.OwnerName => string.IsNullOrEmpty(PhotonNetwork.LocalPlayer.NickName)
+                        ? PhotonNetwork.LocalPlayer.UserId
+                        : PhotonNetwork.LocalPlayer.NickName,
+                    DisplayKind.Custom => RightBaseCharacter.DisplayText,
+                    _ => ""
+                };
+                rightTeamHealthBar.fillAmount = Mathf.Clamp01((float)RightBaseCharacter.HP / RightBaseCharacter.MaxHP);
+            }
         }
     }
 }
